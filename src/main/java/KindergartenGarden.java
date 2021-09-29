696f2ffa9af6b9f9cc33dc4d4f1c75af344f9340
Shazidul Islam <shaziislam417@gmail.com>
2021-09-28 21:28:02 -0400

    first commit

---
+++ src/main/java/KindergartenGarden.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+class KindergartenGarden {
+
+    KindergartenGarden(String garden) {
+        System.out.print(garden);
+        System.out.printf("\n%s", garden.toUpperCase());
+
+    }
+
+    List<Plant> getPlantsOfStudent(String student) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        new KindergartenGarden("testing\nlines");
+    }
+}
